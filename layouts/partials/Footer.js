c131f358e398058614ab3d06172f217ecad86af6
PrachiJadhav07 <prachijadhav742k@gmail.com>
2024-08-12 21:19:32 +0530

    Changes in footer & index

--- layouts/partials/Footer.js
+++ layouts/partials/Footer.js
@@ -46,12 +46,12 @@ const Footer = () => {
           </div>
         </div>
         {/* copyright */}
-        <div className="border-t border-border py-6 bg-cover" style={{ display: "flex", justifyContent: "center", alignItems: "center", background:"#8fc745", color:"black" }}>
+        <div className="border-t border-border py-6 bg-cover" style={{ display: "flex", justifyContent: "center", alignItems: "center", background:"white", color:"black" }}>
           {/* {markdownify(copyright, "p", "text-sm text-center")} */}
-          <p className="site-footer__copy" style={{ display: "flex", alignItems: "center", margin: "0" }}>
+          <p className="site-footer__copy" style={{ display: "flex", alignItems: "center", margin: "0", fontSize: "20px" }}>
             &copy; सर्वहक्कस्वाधीन २०२४ &nbsp;&nbsp;{" "}
               <a href="https://www.swasamvedya.com/" target="_blank">
-                <img src="/images/SIC_LogoName.svg" alt="Logo"  style={{height:"50px", width:"200px"}}/>
+                <img src="/images/SIC_LogoName.svg" alt="Logo"  style={{height:"55px", width:"200px"}}/>
               </ a>
               &nbsp;&nbsp; द्वारे ❤ प्रेमपूर्वक विकसित  
           </p>
--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,72 @@ import { markdownify } from "@lib/utils/textConverter";
 import Image from "next/image";
 import Background from "../public/images/Background.jpg";
 import Link from "next/link";
-import { Autoplay, Pagination } from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/swiper.min.css";
 import { getListPage } from "../lib/contentParser";
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import { useEffect } from "react";
 
 const Home = ({ frontmatter }) => {
   const { banner, feature, services, workflow, call_to_action } = frontmatter;
   const { title } = config.site;
 
+  const ContactHeader = () => {
+    return (
+      <div className="bg-cover bg-center text-center  relative z-10 py-6 mt-12" style={{ backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url('/images/bg (2).jpg')", marginTop: '50px', height: '250px', marginBottom:'50px' }}>
+        <h1 className="text-5xl font-bold leading-tight text-primary absolute bottom-6 left-1/2 transform -translate-x-1/2" style={{bottom: '50%', left: '50%', transform: 'translate(-50%, 50%)', }}>"Gardens are where we find peace and beauty"</h1>
+      </div>
+    );
+  };  
+
+  useEffect(() => {
+    const elements = document.querySelectorAll('.text-animate');
+    
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animate');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, {
+      threshold: 0.1
+    });
+
+    elements.forEach((element) => {
+      observer.observe(element);
+    });
+
+    return () => {
+      elements.forEach((element) => {
+        observer.unobserve(element);
+      });
+    };
+  }, []);
+
   return (
     <Base title={title}>
+      <style jsx>{`
+        @keyframes fadeInUp {
+          0% {
+            opacity: 0;
+            transform: translateY(20px);
+          }
+          100% {
+            opacity: 1;
+            transform: translateY(0);
+          }
+        }
+
+        .text-animate {
+          opacity: 0;
+          transform: translateY(20px);
+          transition: opacity 1.5s ease-in-out, transform 1.5s ease-in-out;
+        }
+
+        .text-animate.animate {
+          animation: fadeInUp 1.5s ease-in-out forwards;
+        }
+      `}</style>
+
       <div className="content">
         {/* Banner */}
         <section
@@ -49,33 +103,17 @@ const Home = ({ frontmatter }) => {
           <div className="container">
             <div className="row text-center">
               <div className="mx-auto lg:col-10">
-                <h1 className="font-primary font-bold" style={{color:"white", animation: "bounceIn 2s ease-in-out 0s 1 normal forwards"}}>{banner.title}</h1>
-                <p className="mt-4" style={{color:'#02fc68', fontWeight:'bold', fontSize:'20px', fontStyle:'italic', animation: "bounceIn 2s ease-in-out 0s 1 normal forwards"}}>{markdownify(banner.content)}</p>
-                <style jsx>{`
-                  @keyframes bounceIn {
-                      0% { transform: scale(0); opacity: 0; }
-                      60% { transform: scale(1.1); opacity: 1; }
-                      80% { transform: scale(0.9); }
-                      100% { transform: scale(1); }
-                  }
-                `}</style>
+                <h1 className="font-primary font-bold text-animate" style={{color:"white"}}>{banner.title}</h1>
+                <p className="mt-4 text-animate" style={{color:'#02fc68', fontWeight:'bold', fontSize:'20px', fontStyle:'italic'}}>{markdownify(banner.content)}</p>
                 {banner.button.enable && (
                   <Link
-                    className="btn btn-primary mt-4"
+                    className="btn btn-primary mt-4 text-animate"
                     href={banner.button.link}
                     rel={banner.button.rel}
                   >
                     {banner.button.label}
                   </Link>
                 )}
-                {/* <Image
-                  className="mx-auto mt-12"
-                  src={banner.image}
-                  width={750}
-                  height={390}
-                  alt="banner image"
-                  priority
-                /> */}
               </div>
             </div>
           </div>
@@ -85,12 +123,12 @@ const Home = ({ frontmatter }) => {
         <section className="section bg-theme-light">
           <div className="container">
             <div className="text-center">
-              <h2>{markdownify(feature.title)}</h2>
+              <h2 className="text-animate">{markdownify(feature.title)}</h2>
             </div>
             <div className="mt-8 grid gap-x-8 gap-y-6 sm:grid-cols-2 lg:grid-cols-3">
               {feature.features.map((item, i) => (
                 <div
-                  className="feature-card rounded-xl bg-white p-5 pb-8 text-center"
+                  className="feature-card rounded-xl bg-white p-5 pb-8 text-center text-animate"
                   key={`feature-${i}`}
                 >
                   {item.icon && (
@@ -115,7 +153,7 @@ const Home = ({ frontmatter }) => {
         {/* services */}
         <div>
           <div className="bg-green-900">
-            <div className="text-center text-white py-16">
+            <div className="text-center text-white py-16 text-animate">
               <h1 className="text-3xl font-bold text-white">What's New in Nature</h1>
               <p className="mt-4">Recent updates, immersive stories, and relevant explainer articles from around The Nature Conservancy.</p>
             </div>
@@ -123,7 +161,7 @@ const Home = ({ frontmatter }) => {
           <div className="relative">
             <div className="bg-green-900 h-[170px] w-full absolute inset-0"></div>
             <div className="relative max-w-6xl mx-auto px-4 md:px-8 grid grid-cols-1 md:grid-cols-2 gap-0">
-              <div className="bg-white text-black p-8 flex flex-col justify-between h-[330px] w-full">
+              <div className="bg-white text-black p-8 flex flex-col justify-between h-[330px] w-full text-animate">
                 <div>
                   <h2 className="text-2xl font-bold mb-2">Gardening is the art that uses flowers and plants as paint, and the soil and sky as canvas.</h2>
                   <p>Flowers and plants as paint suggests that gardeners carefully select and arrange plants much like an artist chooses colors to create a painting. Soil and sky as canvas emphasizes the importance of the natural environment in which gardens thrive.</p>
@@ -139,7 +177,7 @@ const Home = ({ frontmatter }) => {
               <div className="flex justify-center items-center h-[330px] w-full">
                 <img src="/images/G5 new.jpg" alt="Bird's Head" className="w-full h-full object-cover" />
               </div>
-              <div className="bg-white text-black p-8 flex flex-col justify-between h-[330px] w-full">
+              <div className="bg-white text-black p-8 flex flex-col justify-between h-[330px] w-full text-animate">
                 <div>
                   <h2 className="text-2xl font-bold mb-2">Gardening is the purest of human pleasures.</h2>
                   <p>Gardening is the purest of human pleasures underscores the intrinsic joy, satisfaction, and fulfillment that gardening brings. It celebrates the fundamental pleasure derived from nurturing life, connecting with nature, and witnessing the beauty of growth and renewal firsthand.</p>
@@ -148,6 +186,7 @@ const Home = ({ frontmatter }) => {
             </div>
           </div>
         </div>
+        <ContactHeader />
 
         {/* workflow */}
         {/* <section className="section pb-0">
